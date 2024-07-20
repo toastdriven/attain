959cebcabc3272df0ece36fb7f057fa6b4ab09e4
Daniel Lindsley <daniel@toastdriven.com>
2024-07-20 04:13:04 -0500

    Re-added CSV import/export. (#5)

--- tests/test_markov.py
+++ tests/test_markov.py
@@ -1,4 +1,7 @@
+import csv
+from pathlib import Path
 import random
+import shutil
 
 import pytest
 
@@ -20,6 +23,11 @@ def tokenize(sentence):
             tokens.append(cleaned)
 
     return tokens
+
+
+@pytest.fixture
+def data_dir():
+    return Path("/tmp") / "attain_tests"
 
 
 @pytest.fixture
@@ -108,3 +116,117 @@ def test_generate_sentence(small_corpus):
     assert mc.generate_sentence() == "Say hello kitty is my favorite."
     assert mc.generate_sentence() == "Kitty is the question hello kitty is my favorite!"
     assert mc.generate_sentence(min_length=3, max_length=4) == "Not to be that?"
+
+
+def test_to_csv(small_corpus, data_dir):
+    mc = markov.MarkovChain()
+    mc.train(small_corpus)
+
+    csv_path = data_dir / "transitions_matrix.csv"
+
+    shutil.rmtree(data_dir.as_posix(), ignore_errors=True)
+    data_dir.mkdir(parents=True, exist_ok=True)
+    assert csv_path.exists() == False
+
+    mc.to_csv(csv_path.as_posix())
+
+    assert csv_path.exists()
+
+    with open(csv_path.as_posix(), "r") as raw_file:
+        reader = csv.reader(raw_file)
+        headers = next(reader)
+        assert headers == [
+            "",
+            "world",
+            "hello",
+            "say",
+            "to",
+            "be",
+            "or",
+            "not",
+            "that",
+            "is",
+            "the",
+            "question",
+            "kitty",
+            "my",
+            "favorite",
+            "character",
+            "here",
+        ]
+
+        row_0 = next(reader)
+        assert row_0 == [
+            "world",
+            "0.0",
+            "0.0",
+            "0.045454545454545456",
+            "0.0",
+            "0.0",
+            "0.0",
+            "0.0",
+            "0.0",
+            "0.0",
+            "0.0",
+            "0.0",
+            "0.0",
+            "0.0",
+            "0.0",
+            "0.0",
+            "0.0",
+        ]
+        row_1 = next(reader)
+        assert row_1 == [
+            "hello",
+            "0.045454545454545456",
+            "0.0",
+            "0.0",
+            "0.045454545454545456",
+            "0.0",
+            "0.0",
+            "0.0",
+            "0.0",
+            "0.0",
+            "0.0",
+            "0.0",
+            "0.045454545454545456",
+            "0.0",
+            "0.0",
+            "0.0",
+            "0.0",
+        ]
+
+    shutil.rmtree(data_dir.as_posix(), ignore_errors=True)
+
+
+def test_from_csv(small_corpus, data_dir):
+    csv_path = data_dir / "transitions_matrix.csv"
+
+    shutil.rmtree(data_dir.as_posix(), ignore_errors=True)
+    data_dir.mkdir(parents=True, exist_ok=True)
+    assert csv_path.exists() == False
+
+    with open(csv_path.as_posix(), "w") as raw_file:
+        writer = csv.writer(raw_file)
+
+        writer.writerow(["", "hello", "world", "peace"])
+        writer.writerow(["hello", 0.25, 0.0, 0.0])
+        writer.writerow(["world", 0.5, 0.0, 0.0])
+        writer.writerow(["peace", 0.0, 0.25, 0.0])
+
+    mc = markov.MarkovChain.from_csv(csv_path.as_posix())
+    assert len(mc) == 3
+    assert "hello" in mc
+    assert "world" in mc
+    assert "peace" in mc
+
+    # Reach in a little, just to make sure things look right.
+    assert mc._matrix.get_row("world") == [0.5, 0.0, 0.0]
+    assert mc._matrix.get_column("hello") == [0.25, 0.5, 0.0]
+
+    # To keep the randomness consistent.
+    random.seed(a=1)
+
+    assert mc.random_state() == "hello"
+
+    shutil.rmtree(data_dir.as_posix(), ignore_errors=True)
