6e8ece97c50342b128d189fdbd3f659cd2f2f702
Daniel Lindsley <daniel@toastdriven.com>
2024-07-20 03:14:27 -0500

    A bit more on matrix, in prep for integration.

--- src/attain/matrix.py
+++ src/attain/matrix.py
@@ -8,26 +8,28 @@ class SparseMatrix:
 
     Usage::
 
-        >>> mat = SparseMatrix()
+        # The default here is optional (typically `0.0`).
+        >>> mat = SparseMatrix(default=0.9999)
 
         >>> mat.set("a", "a", 1)
         >>> mat.set("a", "b", 0.5)
         >>> mat.set("b", "c", 0.2)
 
-        >>> mat.get("a", "b", default=0.9999)
+        >>> mat.get("a", "b")
         # 0.5
 
     """
 
-    def __init__(self):
+    def __init__(self, default=0.0):
         self._names = []
         self._data = {}
+        self._default = default
 
     def __str__(self):
         return f"{len(self)} unique names"
 
     def __repr__(self):
-        return f"<SparseSquareMatrix: {len(self)}>"
+        return f"<SparseMatrix: {len(self)}>"
 
     def __len__(self):
         return len(self._names)
@@ -35,16 +37,137 @@ class SparseMatrix:
     def __contains__(self, key):
         return key in self._names
 
-    def get(self, x, y, default=0.0):
+    @property
+    def names(self):
+        return self._names
+
+    def x_offset(self, x):
+        """
+        Returns the numerical offset of name `x`.
+
+        Mostly internally useful for determining column names within a row.
+
+        Args:
+            x (str): The column name.
+
+        Returns:
+            int: The column offset.
+        """
+        return self._names.index(x)
+
+    def get(self, x, y):
+        """
+        Fetches the value at a given column/row.
+
+        If not set, returns the default value.
+
+        Args:
+            x (str): The column name.
+            y (str): The row name.
+
+        Returns:
+            any: The value at that location. If not set, returns the default
+                value for the `SparseMatrix`.
+        """
         try:
             x_offset = self._names.index(x)
         except ValueError:
-            return default
+            return self._default
 
         row = self._data.get(y, {})
-        return row.get(x_offset, default)
+        return row.get(x_offset, self._default)
+
+    def get_sparse_row(self, y):
+        """
+        Returns a sparse "row" (dict) for a given row.
+
+        Mostly internal, but there if you need it.
+
+        Args:
+            y (str): The row name.
+
+        Returns:
+            dict: A sparse representation of the row. Keys are **offsets** of the
+                columns, values are the value at locations that are set.
+        """
+        return self._data.get(y, {})
+
+    def get_sparse_column(self, x):
+        """
+        Returns a sparse "column" (dict) for a given column.
+
+        Mostly internal, but there if you need it.
+
+        Args:
+            x (str): The column name.
+
+        Returns:
+            dict: A sparse representation of the column. Keys are **offsets** of
+                the columns, values are the value at locations that are set.
+        """
+        column = {}
+        column_offset = self.x_offset(x)
+
+        for offset, x_name in enumerate(self._names):
+            row = self._data.get(x_name, {})
+
+            if column_offset in row:
+                column[offset] = row[column_offset]
+
+        return column
+
+    def get_row(self, y):
+        """
+        Returns a full row, as if the matrix were fully populated.
+
+        Args:
+            y (str): The row name.
+
+        Returns:
+            list: A full representation of the row, populated with default values
+                where not explicitly set.
+        """
+        actual_row = self.get_sparse_row(y)
+        row = []
+
+        for offset, _ in enumerate(self._names):
+            row.append(actual_row.get(offset, self._default))
+
+        return row
+
+    def get_column(self, x):
+        """
+        Returns a full column, as if the matrix were fully populated.
+
+        Args:
+            x (str): The column name.
+
+        Returns:
+            list: A full representation of the column, populated with default values
+                where not explicitly set.
+        """
+        column = []
+        offset = self.x_offset(x)
+
+        # This is a little slow & kinda-garbage.
+        for x_name in self._names:
+            row = self._data.get(x_name, {})
+            column.append(row.get(offset, self._default))
+
+        return column
 
     def set(self, x, y, value):
+        """
+        Sets a value at a given column/row location.
+
+        Args:
+            x (str): The column name.
+            y (str): The row name.
+            value (any): The value to place at that location.
+
+        Returns:
+            None
+        """
         if x not in self._names:
             self._names.append(x)
 
--- tests/test_matrix.py
+++ tests/test_matrix.py
@@ -59,10 +59,9 @@ def test_small_matrix_internals(small_matrix):
 
 def test_small_matrix_get(small_matrix):
     assert small_matrix.get("hello", "world") == 0.5
-    assert small_matrix.get("hello", "hello") == 0.0
 
     # Not found, return default.
-    assert small_matrix.get("world", "world", 1.0) == 1.0
+    assert small_matrix.get("hello", "hello") == 0.0
 
 
 def test_medium_matrix_internals(medium_matrix):
@@ -88,8 +87,8 @@ def test_medium_matrix_internals(medium_matrix):
 def test_medium_matrix_get(medium_matrix):
     assert medium_matrix.get("a", "a") == 1
     assert medium_matrix.get("a", "b") == 0.5
-    assert medium_matrix.get("b", "b", 1.0) == 0.2
-    assert medium_matrix.get("c", "a", 1.0) == 0.3
+    assert medium_matrix.get("b", "b") == 0.2
+    assert medium_matrix.get("c", "a") == 0.3
     assert medium_matrix.get("c", "d") == 1
     assert medium_matrix.get("d", "c") == 0.6
 
@@ -97,12 +96,45 @@ def test_medium_matrix_get(medium_matrix):
     assert medium_matrix.get("q", "r") == 0.0
 
 
+def test_x_offset(medium_matrix):
+    assert medium_matrix.x_offset("a") == 0
+    assert medium_matrix.x_offset("d") == 3
+
+
+def test_get_sparse_row(medium_matrix):
+    assert medium_matrix.get_sparse_row("a") == {0: 1, 2: 0.3}
+    assert medium_matrix.get_sparse_row("b") == {0: 0.5, 1: 0.2}
+    assert medium_matrix.get_sparse_row("c") == {3: 0.6}
+    assert medium_matrix.get_sparse_row("d") == {2: 1}
+
+
+def test_get_sparse_column(medium_matrix):
+    assert medium_matrix.get_sparse_column("a") == {0: 1, 1: 0.5}
+    assert medium_matrix.get_sparse_column("b") == {1: 0.2}
+    assert medium_matrix.get_sparse_column("c") == {0: 0.3, 3: 1}
+    assert medium_matrix.get_sparse_column("d") == {2: 0.6}
+
+
+def test_get_row(medium_matrix):
+    assert medium_matrix.get_row("a") == [1, 0.0, 0.3, 0.0]
+    assert medium_matrix.get_row("b") == [0.5, 0.2, 0.0, 0.0]
+    assert medium_matrix.get_row("c") == [0.0, 0.0, 0.0, 0.6]
+    assert medium_matrix.get_row("d") == [0.0, 0.0, 1, 0.0]
+
+
+def test_get_column(medium_matrix):
+    assert medium_matrix.get_column("a") == [1, 0.5, 0.0, 0.0]
+    assert medium_matrix.get_column("b") == [0.0, 0.2, 0.0, 0.0]
+    assert medium_matrix.get_column("c") == [0.3, 0.0, 0.0, 1]
+    assert medium_matrix.get_column("d") == [0.0, 0.0, 0.6, 0.0]
+
+
 def test_str(medium_matrix):
     assert str(medium_matrix) == "4 unique names"
 
 
 def test_repr(medium_matrix):
-    assert repr(medium_matrix) == "<SparseSquareMatrix: 4>"
+    assert repr(medium_matrix) == "<SparseMatrix: 4>"
 
 
 def test_len(medium_matrix):
