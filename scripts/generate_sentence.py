36f69a1209d0bbcbe5a0a09f0a88312a4afa3b8b
Daniel Lindsley <daniel@toastdriven.com>
2024-07-24 02:39:51 -0500

    Updated the generate_sentence script to include writing the model & timing.

--- scripts/generate_sentence.py
+++ scripts/generate_sentence.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+import datetime
+from pathlib import Path
 import sys
 
 import attain
@@ -6,7 +8,7 @@ import attain
 
 def tokenize(sentence):
     tokens = []
-    punct = "!@#$%^&*()_+-={}[]\|;':\",.<>\/?"
+    punct = "!@#$%^&*()_+-={}[]\\|;':\",.<>/?"
 
     for word in sentence.split(" "):
         cleaned = word
@@ -21,15 +23,42 @@ def tokenize(sentence):
     return tokens
 
 
+class Timer:
+    def __init__(self):
+        self.start = None
+        self.end = None
+
+    def __enter__(self):
+        self.start = datetime.datetime.now()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.end = datetime.datetime.now()
+
+    def elapsed(self):
+        return self.end - self.start
+
+
 def main(corpus_filename):
+    cache_filename = corpus_filename.parent.joinpath(f"{corpus_filename.stem}.json")
     mc = attain.MarkovChain()
     corpus = []
 
-    with open(corpus_filename, "r") as raw_corpus:
-        for line in raw_corpus:
-            corpus.extend([word for word in tokenize(line)])
+    if cache_filename.exists():
+        mc = attain.MarkovChain.from_json(cache_filename.as_posix())
+    else:
+        with open(corpus_filename.as_posix(), "r") as raw_corpus:
+            for line in raw_corpus:
+                corpus.extend([word for word in tokenize(line)])
 
-    mc.train(corpus)
+        with Timer() as t:
+            mc.train(corpus)
+
+        print(f"Trained in {t.elapsed()}")
+
+        if not cache_filename.exists():
+            mc.to_json(cache_filename.as_posix())
+
     return mc.generate_sentence()
 
 
@@ -38,5 +67,6 @@ if __name__ == "__main__":
         print("Usage: generate_sentence.py <corpus_filename>")
         sys.exit(1)
 
-    sentence = main(sys.argv[1])
+    corpus_filename = Path(sys.argv[1])
+    sentence = main(corpus_filename)
     print(sentence)
